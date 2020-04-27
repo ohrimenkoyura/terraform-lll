a835f2ceaee36ef2afc414ccee4730580a1cdf29
ievo <ohrimneko.yura@gmail.com>
2020-04-27 11:31:23 +0300

    Work with terraform & AWS

---
+++ connections.tf
@@ -0,0 +1,15 @@
+//provider "google" {
+//    credentials = "${file("../account.json") }"
+//    project = "thrashingcorecode"
+//    region = "us-west1"
+//}
+
+provider "aws" {
+    region = "us-east-2"
+}
+//provider "azurerm" {
+//    subscription_id = "0"
+//    client_id = "1"
+//    client_secret = "2"
+//    tenant_id = "3"
+//}
