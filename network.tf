d83207f913e3c5708845722e5c9ed803d26370d5
ievo <ohrimneko.yura@gmail.com>
2020-04-30 12:10:49 +0300

    add google server

--- network.tf
+++ network.tf
@@ -3,13 +3,6 @@ resource "google_compute_network" "our_development_network" {
     auto_create_subnetworks = true
 }
 
-resource "google_compute_subnetwork" "dev-subnet" {
-    ip_cidr_range = "10.0.1.0/24"
-    name = "devsubnet"
-    network = "${google_compute_network.our_development_network.self_link}"
-    region = "us-west1"
-}
-
 resource "aws_vpc" "environment-example-two" {
     cidr_block = "10.0.0.0/16"
     enable_dns_hostnames = "true"
@@ -20,33 +13,10 @@ resource "aws_vpc" "environment-example-two" {
     }
 }
 
-resource "aws_subnet" "subnet1" {
-//    cidr_block = "${aws_vpc.environment-example-two.id}"
-    cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
-    vpc_id = "${aws_vpc.environment-example-two.id}"
-    availability_zone = "us-east-2a"
-}
 
-resource "aws_subnet" "subnet2" {
-    cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
-    vpc_id = "${aws_vpc.environment-example-two.id}"
-    availability_zone = "us-east-2b"
 
-}
 
-resource "aws_security_group" "subnetsecurity" {
-    vpc_id = "${aws_vpc.environment-example-two.id}"
 
-    ingress {
-        cidr_blocks = [
-            "${aws_vpc.environment-example-two.cidr_block}"
-        ]
-        from_port = 80
-        protocol = "tcp"
-        to_port = 80
-    }
-}
-//
 //resource "azurerm_resource_group" "azy_network" {
 //    location = "West US"
 //    name = "devresgrp"
--- servers.tf
+++ servers.tf
@@ -1,26 +1,51 @@
-resource "google_compute_instance" "firstserver" {
-  name = "thefirstserver"
-  machine_type = "n1-standard-1"
-  zone = "us-west1-a"
+//resource "google_compute_instance" "firstserver" {
+//  name = "thefirstserver"
+//  machine_type = "n1-standard-1"
+//  zone = "us-west1-a"
+//
+//  boot_disk {
+//    initialize_params {
+//      image = "debian-cloud/debian-8"
+//    }
+//  }
+//
+//  network_interface {
+//    subnetwork = "${google_compute_subnetwork.dev-subnet.name}"
+//
+//    access_config {
+//    }
+//  }
+//
+//  metadata {
+//    foo = "bar"
+//  }
+//
+//  service_account {
+//    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+//  }
+//}
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-8"
-    }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name= "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trustu-14.04-amd64-server-*"]
   }
 
-  network_interface {
-    subnetwork = "${google_compute_subnetwork.dev-subnet.name}"
-
-    access_config {
-    }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
   }
+//canonical
+  owners = ["099720109477"]
+}
 
-  metadata {
-    foo = "bar"
-  }
+resource "aws_instance" "secondserver" {
+  ami = "${data.aws_ami.ubuntu.id} "
+  instance_type = "t2.micro"
 
-  service_account {
-    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  tags = {
+    name = "identifiertag"
   }
 }
